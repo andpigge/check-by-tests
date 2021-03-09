0453139647df15faa88c5351f2ecab035cf6a775
Rustam <rustamaaaa@bk.ru>
2021-03-09 13:05:00 +0300

    dev

--- javascript/script.js
+++ javascript/script.js
@@ -1,55 +1,55 @@
+/* Массив с тестами */
+const initialTests = [
+  {
+    name: 'Тест по Питеру',
+    desc: 'Насколько хорошо вы знакомы с Питером',
+  },
+  {
+    name: 'Тест по Москве',
+    desc: 'Насколько хорошо вы знакомы с Москвой',
+  },
+];
+
 // Блок create-test
-let createTest = document.querySelector('.create-test');
+const createTest = document.querySelector('.create-test');
 // Данные из формы
-let inputName = createTest.querySelector('.create-test__input-text');
-let inputDesc = createTest.querySelector('.create-test__input-textarea');
+const inputName = createTest.querySelector('.create-test__input-text');
+const inputDesc = createTest.querySelector('.create-test__input-textarea');
 /* Кнопка создать тест  */
-let saveTest = createTest.querySelector('.create-test__save-sb');
+const saveTest = createTest.querySelector('.create-test__save-sb');
 
 /* Блок tests */
-let tests = document.querySelector('.tests');
+const tests = document.querySelector('.tests');
 /* Контейнер со всеми тестами */
-let testsList = tests.querySelector('.tests__list');
+const testsList = tests.querySelector('.tests__list');
 /* Надпись тесты отсуствуют */
-let testsNotification = tests.querySelector('.tests__notification');
+const testsNotification = tests.querySelector('.tests__notification');
+/* Кнопка по которой открывается попап редактирования тестов */
+const btnEditTest = testsList.querySelector('.card-test__edit-btn');
+
+/* Попап редактирования тестов */
+const popupEditTest = document.querySelector('.popup_edit_tests');
+/* Текстовое поле формы, название теста */
+const inputNameTest = document.querySelector('.popup__input-text');
+/* Текстовое поле формы, описание теста */
+const inputDescTest = document.querySelector('.popup__input-textarea');
+
+/* Кнопки закрытия попапов */
+const closePopupBtns = document.querySelectorAll('.popup__close-btn');
+
+/* Шаблон для создания теста */
+const templateCreateTest = document.querySelector('.tests-list-container');
+
 
 /* Блок шапка */
-let header = document.querySelector('.header');
+const header = document.querySelector('.header');
 /* Кнопка открывает скрывает меню на мобильных устройствах */
-let openMenu = header.querySelector('.header__open-menu-btn');
+const openMenu = header.querySelector('.header__open-menu-btn');
 /* Меню */
-let menuButton = header.querySelector('.header__list');
+const menuButton = header.querySelector('.header__list');
 
 /* Проверяет на наличие тестов на странице */
-existTests();
-
-function addTest(inputNameValue, inputDescValue) {
-  console.log(inputNameValue, inputDescValue)
-  let blok =
-  `<li class="tests__item">
-    <div class="card-test">
-      <div class="card-test__container-text">
-        <h2 class="card-test__title">
-          ${inputNameValue}
-        </h2>
-        <p class="card-test__desc">
-          ${inputDescValue}
-        </p>
-      </div>
-      <ul class="card-test__list">
-        <li class="card-test__item">
-          <button class="card-test__edit-btn"></button>
-        </li>
-        <li class="card-test__item">
-          <button class="card-test__delete-btn"></button>
-        </li>
-      </ul>
-    </div>
-  </li>`;
-
-  /* Есть еще insertAdjacentElement */
-  testsList.insertAdjacentHTML('afterbegin', blok);
-}
+/* existTests(); */
 
 /* Сбрасывает форму */
 function clearForm() {
@@ -61,24 +61,22 @@ function clearForm() {
 function formSubmitHandler(event) {
   event.preventDefault();
 
-  let inputNameValue = inputName.value;
-  let inputDescValue = inputDesc.value;
+  const inputNameValue = inputName.value;
+  const inputDescValue = inputDesc.value;
 
   if (inputNameValue.length < 4 || inputDescValue.length < 8) {
     return;
   }
 
-  addTest(inputNameValue, inputDescValue);
-
   clearForm();
 
-  existTests();
+  /* existTests(); */
 }
 
 addEventListener('submit', formSubmitHandler);
 
 /* Проверяет на наличие созданных тестов */
-function existTests() {
+/* function existTests() {
   const TestsList = testsList.querySelectorAll('.tests__item');
 
   if (TestsList.length === 0) {
@@ -86,20 +84,95 @@ function existTests() {
   } else {
     testsNotification.classList.remove('tests__notification_active');
   }
-}
+} */
 
 /* Открыть закрыть меню */
 function toggleMenuModule() {
   menuButton.classList.toggle('header__list_active');
+
+  /* Не дает вводить в форму при открытом меню на мобильных */
+  disabledForm();
 }
 
-addEventListener('click', toggleMenuModule);
+openMenu.addEventListener('click', toggleMenuModule);
 
 /* Запрещает при открытом меню писать в форме и отправлять ее */
-/* function disabledForm() {
+function disabledForm() {
   if (menuButton.classList.contains('header__list_active')) {
     console.log('disabled')
+    inputName.setAttribute('disabled', '');
+    inputDesc.setAttribute('disabled', '');
+    saveTest.setAttribute('disabled', '');
   }
 }
 
-disabledForm() */
+/* Сохряняет данные из формы попапа, в карточку которая редактируется */
+function editTest(btnEdit) {
+  btnEdit.closest('.card-test').querySelector('.card-test__title').textContent = inputNameTest.value;
+  btnEdit.closest('.card-test').querySelector('.card-test__desc').textContent = inputDescTest.value;
+}
+
+/* Обработчик формы редактирования теста */
+function formSubmitHandlerEditTest(btnEdit, event) {
+  console.log(btnEdit)
+  editTest(btnEdit);
+
+  closeAllPopups(event);
+}
+
+/* Сохраняет данные из формы попапа редактирования теста, в тест карточку */
+function editInputValue(event) {
+  const cardNode = event.target.closest('.card-test');
+
+  inputNameTest.value = cardNode.querySelector('.card-test__title').textContent;
+  inputDescTest.value = cardNode.querySelector('.card-test__desc').textContent;
+}
+
+showTests();
+
+/* Выводит начальные тесты из массива, и вешает события */
+function showTests() {
+  initialTests.forEach(test => {
+    const testCard = createTestNode(test.name, test.desc);
+
+    testCard.querySelector('.card-test__edit-btn').addEventListener('click', function(event) {
+      openPopupEditTest();
+      editInputValue(event);
+
+      const btnEdit = event.target;
+      console.log(btnEdit)
+      popupEditTest.addEventListener('submit', (event) => formSubmitHandlerEditTest(btnEdit, event));
+    });
+
+    testsList.append(testCard);
+  });
+}
+
+/* Создает один тест из шаблона. Принимает в параметр заголовок теста, и его описание */
+function createTestNode(titleTest, descTest) {
+  const cardTest = templateCreateTest.content.querySelector('.tests__item').cloneNode('true');
+
+  cardTest.querySelector('.card-test__title').textContent = titleTest;
+  cardTest.querySelector('.card-test__desc').textContent = descTest;
+
+  return cardTest;
+}
+
+/* Открытие попапа редактирования теста */
+function openPopupEditTest() {
+  addPopupClass(popupEditTest);
+}
+
+/* Добавляет класс открытия попапа. Принимает попап аргументом, на который будет вешатся класс открытия попапа */
+function addPopupClass(popupType) {
+  popupType.classList.add('popup_opened')
+}
+
+/* Закрывает открывшейся попап при нажатии на крестик, или кнопку сохранить */
+function closeAllPopups(event) {
+  event.target.closest('.popup').classList.remove('popup_opened');
+}
+
+closePopupBtns.forEach(item => {
+  item.addEventListener('click', closeAllPopups);
+});
